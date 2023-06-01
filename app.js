9f72bbcaeed322db0ef41c97e2179c2d76d91edc
Luca Coduri <coduri.vitor@gmail.com>
2023-06-01 09:34:09 +0200

    https

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require("fs");
 const express = require("express");
+const https = require("https");
 const app = express();
 const port = 3000;
 
@@ -6,6 +8,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("localhost.key"),
+      cert: fs.readFileSync("localhost.crt"),
+    },
+    app,
+  )
+  .listen(3000, () => {
+    console.log("server is runing at port 3000");
+  });
