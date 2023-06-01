72140bfbe34e2adf181e6c4719d3fa9f920b8048
damianomondaini <damiano.mondaini@gmail.com>
2023-06-01 09:41:41 +0200

    certs

--- web/app.js
+++ web/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
+const httpsServer = https.createServer({
+    key: fs.readFileSync('./certs/localhost.key'),
+    cert: fs.readFileSync('./certs/localhost.crt')
+}, app);
 const port = 8080;
 
 app.use(cors());
@@ -14,6 +20,6 @@ app.get('/', (req, res) => {
     res.sendFile('./static/index.html', {root: __dirname});
 });
 
-app.listen(port, () => {
+httpsServer.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
