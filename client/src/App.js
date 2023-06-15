e6241bfbbf726ca50615c087270162a262b7bcfe
hadrylouis <hadry.louis@gmail.com>
2023-06-15 08:27:52 +0200

    [WIP] remove express create home page

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,9 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import sdk from "matrix-js-sdk";
-
 import Login from "./page/Login";
 import Rooms from "./page/Rooms";
+import Home from "./page/Home";
 
 class App extends React.Component {
 
@@ -32,12 +31,15 @@ class App extends React.Component {
     const { setClient } = this;
 
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login client={client} setClient={setClient} />} />
-          <Route path="/rooms" element={!client ? <Navigate to="/login" /> : <Rooms client={client} />} />
-        </Routes>
-      </BrowserRouter>
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login client={client} setClient={setClient} />} />
+            <Route path="/rooms" element={!client ? <Navigate to="/login" /> : <Rooms client={client} />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     )
   }
 }
