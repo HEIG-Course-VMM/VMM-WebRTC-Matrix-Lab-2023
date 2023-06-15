dad709c42b7a35830a7c04a722e4a01d268e6ac3
hadrylouis <hadry.louis@gmail.com>
2023-06-15 09:07:00 +0200

    [WIP] room page

--- client/src/page/Room.js
+++ client/src/page/Room.js
@@ -1,6 +1,87 @@
 import React from "react";
+import sdk from "matrix-js-sdk";
 
 class Room extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.call = null;
+  }
+
+  disableButtons = (place, answer, hangup)  => {
+    document.getElementById("hangup").disabled = hangup;
+    document.getElementById("answer").disabled = answer;
+    document.getElementById("call").disabled = place;
+  }
+
+  addListeners = (call) => {
+    let lastError = "";
+    call.on("hangup", function () {
+        this.disableButtons(false, true, true);
+        document.getElementById("result").innerHTML = "<p>Call ended. Last error: " + lastError + "</p>";
+    });
+    call.on("error", function (err) {
+        lastError = err.message;
+        call.hangup();
+        this.disableButtons(false, true, true);
+    });
+    call.on("feeds_changed", function (feeds) {
+        const localFeed = feeds.find((feed) => feed.isLocal());
+        const remoteFeed = feeds.find((feed) => !feed.isLocal());
+
+        const remoteElement = document.getElementById("remote");
+        const localElement = document.getElementById("local");
+
+        if (remoteFeed) {
+            remoteElement.srcObject = remoteFeed.stream;
+            remoteElement.play();
+        }
+        if (localFeed) {
+            localElement.muted = true;
+            localElement.srcObject = localFeed.stream;
+            localElement.play();
+        }
+    });
+  }
+
+  syncComplete = () => {
+    document.getElementById("result").innerHTML = "<p>Ready for calls.</p>";
+    this.disableButtons(false, true, true);
+
+    document.getElementById("call").onclick = function () {
+        console.log("Placing call...");
+        this.call = sdk.createNewMatrixCall(client, ROOM_ID);
+        console.log("Call => %s", this.call);
+        this.addListeners(this.call);
+        this.call.placeVideoCall();
+        document.getElementById("result").innerHTML = "<p>Placed call.</p>";
+        this.disableButtons(true, false, false);
+    };
+
+    document.getElementById("hangup").onclick = function () {
+        console.log("Hanging up call...");
+        console.log("Call => %s", this.call);
+        this.call.hangup();
+        document.getElementById("result").innerHTML = "<p>Hungup call.</p>";
+    };
+
+    document.getElementById("answer").onclick = function () {
+        console.log("Answering call...");
+        console.log("Call => %s", this.call);
+        this.call.answer();
+        this.disableButtons(true, true, false);
+        document.getElementById("result").innerHTML = "<p>Answered call.</p>";
+    };
+
+    const { client } = this.props;
+    client.on("Call.incoming", function (c) {
+        console.log("Call ringing");
+        this.disableButtons(true, false, false);
+        document.getElementById("result").innerHTML = "<p>Incoming call...</p>";
+        this.call = c;
+        this.addListeners(this.call);
+    });
+  }
 
   render() {
     return (
